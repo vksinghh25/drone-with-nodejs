9dd8ea1262620bf92aba706063ffcce5b31dc0a7
Vipan K Singh <vksinghh@ip-192-168-0-92.ap-south-1.compute.internal>
2020-09-30 10:24:21 +0530

    minor change

--- spec.js
+++ spec.js
@@ -12,7 +12,7 @@ describe('loading express', function () {
     .get('/')
     .expect(200, done);
   });
-  it('404 everything else', function testPath(done) {
+  it('404 everything else and more', function testPath(done) {
     request(server)
       .get('/foo/bar')
       .expect(404, done);
